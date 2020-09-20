1234c4f2f781d673254798b63270c06381e0c51f
Hadi Aji <hadeaji@gmail.com>
2020-09-20 20:38:16 +0300

    Update app.js

--- java/app.js
+++ java/app.js
@@ -21,26 +21,26 @@ this.timesDisplayed = 0;
 prodectsArray.push(this);
 }
 
-new Product('Bag','../img/bag.jpg');
-new Product('Banana','../img/banana.jpg');
-new Product('Bathroom','../img/bathroom.jpg');
-new Product('Boots','../img/boots.jpg');
-new Product('Breakfast','../img/breakfast.jpg');
-new Product('Bubblegum','../img/bubblegum.jpg');
-new Product('Chair','../img/chair.jpg');
-new Product('Cthulhu','../img/cthulhu.jpg');
-new Product('Dog-Duck','../img/dog-duck.jpg');
-new Product('Dragon','../img/dragon.jpg');
-new Product('pen','../img/pen.jpg');
-new Product('Pet-sweep','../img/pet-sweep.jpg');
-new Product('Scissors','../img/scissors.jpg');
-new Product('Shark','../img/shark.jpg');
-new Product('Sweep','../img/sweep.png');
-new Product('Tauntaun','../img/tauntaun.jpg');
-new Product('Unicorn','../img/unicorn.jpg');
-new Product('USB','../img/usb.gif');
-new Product('Water-Can','../img/water-can.jpg');
-new Product('Wine-Glass','../img/wine-glass.jpg');
+new Product('Bag','img/bag.jpg');
+new Product('Banana','img/banana.jpg');
+new Product('Bathroom','img/bathroom.jpg');
+new Product('Boots','img/boots.jpg');
+new Product('Breakfast','img/breakfast.jpg');
+new Product('Bubblegum','img/bubblegum.jpg');
+new Product('Chair','img/chair.jpg');
+new Product('Cthulhu','img/cthulhu.jpg');
+new Product('Dog-Duck','img/dog-duck.jpg');
+new Product('Dragon','img/dragon.jpg');
+new Product('pen','img/pen.jpg');
+new Product('Pet-sweep','img/pet-sweep.jpg');
+new Product('Scissors','img/scissors.jpg');
+new Product('Shark','img/shark.jpg');
+new Product('Sweep','img/sweep.png');
+new Product('Tauntaun','img/tauntaun.jpg');
+new Product('Unicorn','img/unicorn.jpg');
+new Product('USB','img/usb.gif');
+new Product('Water-Can','img/water-can.jpg');
+new Product('Wine-Glass','img/wine-glass.jpg');
 
 
 function imageIndex(){

1aa231be565ce3feb56b4932a834c0b3134da27d
Hadi Aji <hadeaji@gmail.com>
2020-09-24 23:35:51 +0300

    comminting on code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BusMall
+my busmall detect 25 votes, update and preview the number of votes and time displayed immediately and saves them to local storage after you finish the 25 votes.
+it will also remove the 3 options and shows a clear chart about the whole date you have till no
+
--- java/app.js
+++ java/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// my global vars
 var firstImage = document.getElementById('firstImage');
 var SeconedImage = document.getElementById('SeconedImage');
 var ThirdImage = document.getElementById('ThirdImage');
@@ -7,16 +7,12 @@ var imagesDiv = document.getElementById('sec');
 var finalmessage = document.getElementById('finalResult');
 var buttom = document.getElementById('button1');
 
-
-var nowadaysArray = [-1,-1,-1];
+var nowadaysArray = [-1, -1, -1];
 var prodectsArray = [];
-
-
 
 var chartnames = [];
 var chartdisplay = [];
 var chartvotes = [];
-
 
 var currentFirstImage;
 var currentSeconedImage;
@@ -24,190 +20,181 @@ var currentThirdImage;
 var totalClicks = 0;
 
 
-
-function Product (prodectName,link){
-        this.prodectName = prodectName;
-        this.link = link;
-        this.votes = 0;
-        this.timesDisplayed = 0;
-        prodectsArray.push(this);
-        chartnames.push(this.prodectName);
+// main const
+function Product(prodectName, link) {
+  this.prodectName = prodectName;
+  this.link = link;
+  this.votes = 0;
+  this.timesDisplayed = 0;
+  prodectsArray.push(this);
+  chartnames.push(this.prodectName);
 
 }
+// all obects
+new Product('Bag', 'img/bag.jpg');
+new Product('Banana', 'img/banana.jpg');
+new Product('Bathroom', 'img/bathroom.jpg');
+new Product('Boots', 'img/boots.jpg');
+new Product('Breakfast', 'img/breakfast.jpg');
+new Product('Bubblegum', 'img/bubblegum.jpg');
+new Product('Chair', 'img/chair.jpg');
+new Product('Cthulhu', 'img/cthulhu.jpg');
+new Product('Dog-Duck', 'img/dog-duck.jpg');
+new Product('Dragon', 'img/dragon.jpg');
+new Product('pen', 'img/pen.jpg');
+new Product('Pet-sweep', 'img/pet-sweep.jpg');
+new Product('Scissors', 'img/scissors.jpg');
+new Product('Shark', 'img/shark.jpg');
+new Product('Sweep', 'img/sweep.png');
+new Product('Tauntaun', 'img/tauntaun.jpg');
+new Product('Unicorn', 'img/unicorn.jpg');
+new Product('USB', 'img/usb.gif');
+new Product('Water-Can', 'img/water-can.jpg');
+new Product('Wine-Glass', 'img/wine-glass.jpg');
 
-new Product('Bag','img/bag.jpg');
-new Product('Banana','img/banana.jpg');
-new Product('Bathroom','img/bathroom.jpg');
-new Product('Boots','img/boots.jpg');
-new Product('Breakfast','img/breakfast.jpg');
-new Product('Bubblegum','img/bubblegum.jpg');
-new Product('Chair','img/chair.jpg');
-new Product('Cthulhu','img/cthulhu.jpg');
-new Product('Dog-Duck','img/dog-duck.jpg');
-new Product('Dragon','img/dragon.jpg');
-new Product('pen','img/pen.jpg');
-new Product('Pet-sweep','img/pet-sweep.jpg');
-new Product('Scissors','img/scissors.jpg');
-new Product('Shark','img/shark.jpg');
-new Product('Sweep','img/sweep.png');
-new Product('Tauntaun','img/tauntaun.jpg');
-new Product('Unicorn','img/unicorn.jpg');
-new Product('USB','img/usb.gif');
-new Product('Water-Can','img/water-can.jpg');
-new Product('Wine-Glass','img/wine-glass.jpg');
 
 
-
-if (localStorage.getItem('JSONproducts')){
-    prodectsArray = [];
-    prodectsArray = JSON.parse(localStorage.getItem('JSONproducts'));
+if (localStorage.getItem('JSONproducts')) {
+  prodectsArray = [];
+  prodectsArray = JSON.parse(localStorage.getItem('JSONproducts'));
 }
 
-function imageIndex(){
-    var firstImageIndex;
-    var SeconedImageIndex;
-    var ThirdImageIndex;
-
-    
-    
-    do{
+// no images repeat
+function imageIndex() {
+  var firstImageIndex;
+  var SeconedImageIndex;
+  var ThirdImageIndex;
+  do {
     firstImageIndex = Math.floor((Math.random() * prodectsArray.length));
-    }while(firstImageIndex === nowadaysArray[0] || firstImageIndex === nowadaysArray[1] || firstImageIndex === nowadaysArray[2]);
+  } while (firstImageIndex === nowadaysArray[0] || firstImageIndex === nowadaysArray[1] || firstImageIndex === nowadaysArray[2]);
 
-    do{
-        SeconedImageIndex = Math.floor((Math.random() * prodectsArray.length));
-    } while(SeconedImageIndex === firstImageIndex || SeconedImageIndex === nowadaysArray[0] || SeconedImageIndex === nowadaysArray[1] || SeconedImageIndex === nowadaysArray[2]);
+  do {
+    SeconedImageIndex = Math.floor((Math.random() * prodectsArray.length));
+  } while (SeconedImageIndex === firstImageIndex || SeconedImageIndex === nowadaysArray[0] || SeconedImageIndex === nowadaysArray[1] || SeconedImageIndex === nowadaysArray[2]);
 
-    do{
-        ThirdImageIndex = Math.floor((Math.random() * prodectsArray.length));
-    } while(ThirdImageIndex === firstImageIndex || ThirdImageIndex === SeconedImageIndex || ThirdImageIndex === nowadaysArray[0] || ThirdImageIndex === nowadaysArray[1] || ThirdImageIndex === nowadaysArray[2]);
+  do {
+    ThirdImageIndex = Math.floor((Math.random() * prodectsArray.length));
+  } while (ThirdImageIndex === firstImageIndex || ThirdImageIndex === SeconedImageIndex || ThirdImageIndex === nowadaysArray[0] || ThirdImageIndex === nowadaysArray[1] || ThirdImageIndex === nowadaysArray[2]);
 
-    nowadaysArray = [];
-    nowadaysArray.push(firstImageIndex);
-    nowadaysArray.push(SeconedImageIndex);
-    nowadaysArray.push(ThirdImageIndex);
+  nowadaysArray = [];
+  nowadaysArray.push(firstImageIndex);
+  nowadaysArray.push(SeconedImageIndex);
+  nowadaysArray.push(ThirdImageIndex);
 
 
 
-    displayImages(firstImageIndex,SeconedImageIndex,ThirdImageIndex);
-    
+  displayImages(firstImageIndex, SeconedImageIndex, ThirdImageIndex);
+
 }
+// timeDisplayed calc
+function displayImages(first, seconed, third) {
+  currentFirstImage = prodectsArray[first];
+  currentFirstImage.timesDisplayed++;
 
-function displayImages(first, seconed, third){
-    currentFirstImage = prodectsArray[first];
-    currentFirstImage.timesDisplayed++;
+  currentSeconedImage = prodectsArray[seconed];
+  currentSeconedImage.timesDisplayed++;
 
-    currentSeconedImage = prodectsArray[seconed];
-    currentSeconedImage.timesDisplayed++;
+  currentThirdImage = prodectsArray[third];
+  currentThirdImage.timesDisplayed++;
 
-    currentThirdImage = prodectsArray[third];
-    currentThirdImage.timesDisplayed++;
-  
-    firstImage.setAttribute('src',currentFirstImage.link);
-    SeconedImage.setAttribute('src',currentSeconedImage.link);
-    ThirdImage.setAttribute('src',currentThirdImage.link);
+  firstImage.setAttribute('src', currentFirstImage.link);
+  SeconedImage.setAttribute('src', currentSeconedImage.link);
+  ThirdImage.setAttribute('src', currentThirdImage.link);
 }
 
 imageIndex();
+// voting method
+imagesDiv.addEventListener('click', voicesOfTheCustomers);
 
-imagesDiv.addEventListener('click',voicesOfTheCustomers)
+function voicesOfTheCustomers(event) {
+  var clickedImage;
 
-function voicesOfTheCustomers(event){
-    var clickedImage;
+  if (event.target.id === 'firstImage') {
+    clickedImage = currentFirstImage;
+  } else if (event.target.id === 'SeconedImage') {
+    clickedImage = currentSeconedImage;
+  } else if (event.target.id === 'ThirdImage') {
+    clickedImage = currentThirdImage;
+  }
+  finalmessage.textContent = '';
+  displayResults();
 
-    if(event.target.id === 'firstImage'){
-      clickedImage = currentFirstImage;
-    } else if(event.target.id === 'SeconedImage'){
-      clickedImage = currentSeconedImage;
-    } else if(event.target.id === 'ThirdImage'){
-        clickedImage = currentThirdImage;
-}
-    finalmessage.textContent = '';
-    displayResults();
+  clickedImage.votes++;
+  imageIndex();
+  totalClicks++;
 
-    clickedImage.votes++;
-    imageIndex();
-    totalClicks++;
+  if (totalClicks >= 25) {
+    imagesDiv.removeEventListener('click', voicesOfTheCustomers);
+    // displayResults();
+    localStorage.setItem('JSONproducts', JSON.stringify(prodectsArray));
 
-    if(totalClicks >= 25){
-        imagesDiv.removeEventListener('click',voicesOfTheCustomers);
-        // displayResults();
-        localStorage.setItem('JSONproducts', JSON.stringify(prodectsArray));
-
-         // chart info
+    // chart info
     chartdisplay = [];
     chartvotes = [];
-    for(var i = 0; i < prodectsArray.length ; i++){
-        chartdisplay.push(prodectsArray[i].timesDisplayed);
+    for (var i = 0; i < prodectsArray.length; i++) {
+      chartdisplay.push(prodectsArray[i].timesDisplayed);
     }
-    for(var i = 0; i < prodectsArray.length ; i++){
-        chartvotes.push(prodectsArray[i].votes);
+    for (var i = 0; i < prodectsArray.length; i++) {
+      chartvotes.push(prodectsArray[i].votes);
     }
     ///
-    
-
-
-
 
     // hide the 3 images
-    var Element = document.querySelector("#sec");
-    Element.style.display = "none";
+    var Element = document.querySelector('#sec');
+    Element.style.display = 'none';
     //
 
     // show the chart
-    var myElement = document.querySelector("#myChart");
-    myElement.style.display = "visible";
+    var myElement = document.querySelector('#myChart');
+    myElement.style.display = 'visible';
     //
 
 
     var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'bar',
 
-    // The data for our dataset
-    data: {
+      // The data for our dataset
+      data: {
         labels: chartnames,
         datasets: [{
-            label: 'Dispaly Times',
-            backgroundColor: '#F2A477',
-            borderColor: '#F2A477',
-            data: chartdisplay
+          label: 'Dispaly Times',
+          backgroundColor: '#F2A477',
+          borderColor: '#F2A477',
+          data: chartdisplay
         },
         {
-            label: 'Voting Times',
-            backgroundColor: '#59372F',
-            borderColor: '#59372F',
-            data: chartvotes
+          label: 'Voting Times',
+          backgroundColor: '#59372F',
+          borderColor: '#59372F',
+          data: chartvotes
         }]
-    },
+      },
 
-    // Configuration options go here
-    options: {}
-});
+      // Configuration options go here
+      options: {}
+    });
     //
-    }
-
+  }
 }
 
-function displayResults(){
-    var listItem;
-    for(var i = 0; i < prodectsArray.length; i++){
-      listItem = document.createElement('li');
-      listItem.textContent = 'Displayed Times for '+ prodectsArray[i].prodectName + ' is ' + prodectsArray[i].timesDisplayed + ' and votes are ' + prodectsArray[i].votes;
-      finalmessage.appendChild(listItem);
-
-
-    }
-        
-
+// refresh the results
+function displayResults() {
+  var listItem;
+  for (var i = 0; i < prodectsArray.length; i++) {
+    listItem = document.createElement('li');
+    listItem.textContent = 'Displayed Times for ' + prodectsArray[i].prodectName + ' is ' + prodectsArray[i].timesDisplayed + ' and votes are ' + prodectsArray[i].votes;
+    finalmessage.appendChild(listItem);
+  }
 }
+//
 
-buttom.addEventListener('click',ahead)
+buttom.addEventListener('click', ahead);
 
 // buttom clears local
-function ahead(event){
-    if(event.target.id === 'button1'){
-        localStorage.clear();
-    }
+function ahead(event) {
+  if (event.target.id === 'button1') {
+    localStorage.clear();
+  }
 }
